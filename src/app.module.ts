9b07d111c0f9fff1c6feb29550014e86b165367a
Fabrício Milanez <fabricio.milanez@cobmais.com.br>
2025-10-24 12:07:14 -0300

    fix: Adequar a aplicação para ser cross-platform
    Removida a obrigatoriedade de atribuir a variável de ambiente `DATASOURCE` para definir a caminho até o arquivo de dados do SQLite.
    
    Removida a dependência de desenvolvimento `cross-env`. Não resolvia o problema pois o PowerShell não suporta command substitution como outros shells `$( ... )`.
    
    Obrigado Bill Gates 🙏🪟

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { MoviesModule } from './movies/movies.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Movie } from './movies/entities';
+import { Defaults } from './constants/constants';
 
 @Module({
   imports: [
     MoviesModule,
     TypeOrmModule.forRoot({
       type: 'sqlite',
-      database: process.env.DATASOURCE,
+      database: process.env.DATASOURCE || Defaults.DATASOURCE,
       synchronize: false,
       cache: false,
       entities: [Movie],
